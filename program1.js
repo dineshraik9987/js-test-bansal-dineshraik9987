3887683266be8bcd3e4dc1c9f32d8d46ac685739
dineshraik9987 <124186409+dineshraik9987@users.noreply.github.com>
2024-02-23 13:42:56 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,6 +3,30 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    class Solution(object):
+    def isValid(s: str) -> bool:
+
+        """
+        :type s: str
+        :rtype: bool
+        """
+        pass
+    
+    stack = []
+    mapping = {')': '(', '}': '{', ']': '['}
+    for char in s:
+        if char in mapping:
+            top_element = stack.pop() if stack else '#'
+            if mapping[char] != top_element:
+                return False
+        else:
+            stack.append(char)
+    return not stack
+print(isValid("()"))       
+print(isValid("()[]{}"))   
+print(isValid("(]"))        
+
+
     
 };
 
